afa7f9e203843146dceed14dd6b118baf95a62bd
KevinAd92 <105957644+KevinAd92@users.noreply.github.com>
2024-09-23 14:29:12 -0500

    finished

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -75,9 +75,10 @@ public class JobData {
 
             String aValue = row.get(column);
 
-            if (aValue.contains(value)) {
+            if (aValue.toLowerCase().contains(value.toLowerCase())) {
                 jobs.add(row);
             }
+
         }
 
         return jobs;
@@ -95,7 +96,18 @@ public class JobData {
         loadData();
 
         // TODO - implement this method
-        return null;
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        for (HashMap<String, String> row : allJobs) {
+            for (String entry : row.values()) {
+                if (entry.toLowerCase().contains(value.toLowerCase())) {
+                    if (!jobs.contains(row)) {
+                        jobs.add((row));
+                    }
+                }
+            }
+        }
+        return jobs;
     }
 
     /**
